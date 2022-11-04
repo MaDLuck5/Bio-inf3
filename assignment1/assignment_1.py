d7310f6326ad6aef7f399914e7b79a7d255515e6
MaDLuck5 <86706711+MaDLuck5@users.noreply.github.com>
2022-11-04 13:14:32 +0100

    minor bug fixes

--- assignment1/assignment_1.py
+++ assignment1/assignment_1.py
@@ -9,6 +9,7 @@ from Bio import Seq, SeqIO, AlignIO
 from Bio.Seq import Seq
 import sys
 from Bio.Align.Applications import ClustalwCommandline
+from typing import List
 
 
 def alignment_score(msa_alignment, flag=False):
@@ -46,10 +47,12 @@ def generate_cost(mutated_aa_seq, scoring):
     :param scoring:
     :return:
     """
+    counter = 0
     cost = 0
     position_cost = list()
-
+    change_dict = {}
     for aa, d in zip(mutated_aa_seq, scoring):
+        counter += 1
         keys = list(d.keys())
 
         pre_cost = cost
@@ -58,16 +61,30 @@ def generate_cost(mutated_aa_seq, scoring):
             cost += d[keys[0]]
             if len(d) > 1:
                 for key in keys:
+                    if aa not in change_dict:
+                        change_dict[aa] = [key, round(d[key]*100, ndigits=2)]
+                    else:
+                        change_dict[aa].append(key)
+                        change_dict[aa].append(round(d[key]*100, ndigits=2))
                     if aa == key:
                         cost -= d[key]
+            else:
+                if keys[0] not in change_dict:
+                    change_dict[aa] = [keys[0], d[keys[0]] * 100]
+                else:
+                    change_dict[aa] += [keys[0], d[keys[0]] * 100]
 
         position_cost.append(cost - pre_cost)
 
     # for i, e in enumerate(position_cost):
     # print("Position {} has a cost of {}".format(i, e))
     print(f"lengthe of given mutated Sequence ={len(mutated_aa_seq)}")
-    print(f"The total cost for the mutated sequence is: {round(cost)}")
-    #print(f"{round((cost / len(mutated_aa_seq)) * 100)}% conservation compared to the Homology MSA file given")
+    print(f"The total cost for the mutated sequence is: {round(cost, ndigits=2)}")
+    if cost >= 0.8:
+        print(f"cost more than 1 a Amino acid has been changed in a highly conserved place")
+        for key in change_dict:
+            print(f"the new Amino Acid: {key}")
+            print(f"The changed Amino Acid and conservation in % :{change_dict[key]}")
 
 
 def alignment(in_file, out_file_dir, num):
@@ -126,6 +143,13 @@ def fasta_reader(path_to_file):
 
 
 def fasta_altere(fasta_object, point, mutation):
+    """
+
+    :param fasta_object:
+    :param point:
+    :param mutation:
+    :return:
+    """
     sequence = fasta_object.seq
     fasta_object.seq = single_point_mutator(sequence, point, mutation)
     return fasta_object
@@ -184,6 +208,9 @@ def command_line_parsing():
                         help="if TRUE is given, programs shows the conservation of each amino acid in the MSA in "
                              "percentages")
 
+    parser.add_argument("-save", "--create_new_msa", type=bool, default=False,
+                        help="if True is given will creat new MSA with the mutated sequence in the out put folder")
+
     args = parser.parse_args()
     return args
 
@@ -199,23 +226,20 @@ def main():
     working_dir = os.getcwd()
     output_file_dir = working_dir + '\\output'
 
-    normal_fasta = fasta_reader(args.sequence)
-    normal_fasta.seq = nuc_translator_to_aa(normal_fasta.seq)
-    normal_fasta.seq = start_annotation_stripper(normal_fasta.seq)
     mutated_fasta = fasta_altere(fasta_reader(args.sequence), args.location, args.replacement)
     mutated_fasta.seq = nuc_translator_to_aa(mutated_fasta.seq)
 
-    # fasta_list.append(normal_fasta)
-    fasta_list.append(mutated_fasta)
-    fasta_list.extend(multi_fasta_parser(args.file))
+    if args.create_new_msa:
 
-    new_multifasta_path = fasta_writer(fasta_list, output_file_dir)
-    msa_01 = alignment(new_multifasta_path, output_file_dir, 1)
+        fasta_list.append(mutated_fasta)
+        fasta_list.extend(multi_fasta_parser(args.file))
+        new_multifasta_path = fasta_writer(fasta_list, output_file_dir)
+        alignment(args.file, output_file_dir, " new+snp")
+
     msa_02 = alignment(args.file, output_file_dir, 2)
 
-    scoring = alignment_score(msa_01, args.percentage_disp)
     scoring2 = alignment_score(msa_02, args.percentage_disp)
-    generate_cost(mutated_fasta.seq, scoring)
+
     generate_cost(mutated_fasta.seq, scoring2)
 
     return 0
